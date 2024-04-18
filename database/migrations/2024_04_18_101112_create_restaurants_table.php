913df93e4372fdc31648b9fdc15c2b471ad9366a
giamaf <gianluca.maffucci@hotmail.com>
2024-04-18 13:21:12 +0200

    added Restaurant table

---
+++ database/migrations/2024_04_18_101112_create_restaurants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('activity_name', 50)->unique();
+            $table->string('address', 100);
+            $table->char('vat', 11)->unique();
+            $table->string('email', 100)->unique();
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
