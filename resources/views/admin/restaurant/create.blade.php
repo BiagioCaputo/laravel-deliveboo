7c4db294aeab98f8a26145bfff926e4a2bffb446
giamaf <gianluca.maffucci@hotmail.com>
2024-04-20 02:15:51 +0200

    added minor style fixes

--- resources/views/admin/restaurant/create.blade.php
+++ resources/views/admin/restaurant/create.blade.php
@@ -1,27 +1,25 @@
-{{--Layout--}}
+{{-- Layout --}}
 @extends('layouts.app')
 
-{{--Titolo--}}
+{{-- Titolo --}}
 @section('title', 'New_Restaurant')
 
-{{--Contenuto principale pagina--}}
+{{-- Contenuto principale pagina --}}
 @section('content')
 
-<header>
-    <h1 class="text-center my-5">Inserisci i dati del tuo Ristorante</h1>
-</header>
+    <header>
+        <h1 class="text-center my-5">Inserisci i dati del tuo Ristorante</h1>
+    </header>
 
-<main>
-    {{--Form per la modifica di un progetto--}}
+    {{-- Form per la modifica di un progetto --}}
     <div class="container py-5">
-        @include('includes.restaurant.form')      
+        @include('includes.restaurant.form')
     </div>
-</main>
 
 @endsection
 
 
-{{--Scripts--}}
+{{-- Scripts --}}
 @section('scripts')
     {{-- @vite('resources/js/slug_field.js') --}}
     @vite('resources/js/image_preview.js')
--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -1,66 +1,53 @@
-
-{{--Layout--}}
+{{-- Layout --}}
 @extends('layouts.app')
 
-{{--Titolo--}}
+{{-- Titolo --}}
 @section('title', 'My restaurant')
 
 
-{{--Contenuto principale pagina--}}
+{{-- Contenuto principale pagina --}}
 
 @section('content')
 
-<header>
-    <h1 class="text-center my-5">{{ $restaurant->activity_name }}</h1>
-</header>
-<main>
+    <header>
+        <h1 class="text-center my-5">{{ $restaurant->activity_name }}</h1>
+    </header>
     <div class="container py-5">
         <div class="clearfix">
             @if ($restaurant->image)
-                <img src="{{ $restaurant->printImage() }}" alt="{{$restaurant->activity_name}}" class="me-2 float-start">
+                <img src="{{ $restaurant->printImage() }}" alt="{{ $restaurant->activity_name }}" class="me-2 float-start">
             @endif
             <ul class="list-unstyled">
-                <li>{{$restaurant->address}}</li>
-                <li>{{$restaurant->vat}}</li>
-                <li>{{$restaurant->email}}</li>
+                <li>{{ $restaurant->address }}</li>
+                <li>{{ $restaurant->vat }}</li>
+                <li>{{ $restaurant->email }}</li>
             </ul>
             <div class="d-flex justify-content-between">
                 <div>
-                    <span class="me-2"><strong>Creato il:</strong> {{ $restaurant->getFormattedDate('created_at')}}</span>
-                    <span><strong>Modificato il:</strong> {{ $restaurant->getFormattedDate('updated_at')}}</span>
+                    <span class="me-2"><strong>Creato il:</strong>
+                        {{ $restaurant->getFormattedDate('created_at') }}</span>
+                    <span><strong>Modificato il:</strong> {{ $restaurant->getFormattedDate('updated_at') }}</span>
                 </div>
                 <div class="d-flex gap-2">
                     @forelse($restaurant->types as $type)
-                    <span>{{$type->label}}</span>
+                        <span>{{ $type->label }}</span>
                     @empty
-                       -
-                    @endforelse 
+                        -
+                    @endforelse
                 </div>
             </div>
-            
-        </div>
-        <hr>      
-    </div>
-</main>
 
-<footer>
-    <div class="container py-5 d-flex justify-content-end align-items-center">
-        <div>
-            <a href="{{ route('admin.restaurant.edit', $restaurant)}}" class="btn btn-warning"><i class="fas fa-pencil me-2"></i>Modifica</a>
-            {{--      DA IMPLEMENTARE ELIMINAZIONE     --}}
-            {{-- <form action="{{route('admin.restaurant.destroy', $restaurant)}}" method="POST" class="delete-form">
-            @csrf
-            @method('DELETE')
-            <button type='submit' class="btn btn-danger"><i class="fas fa-trash me-2"></i>Elimina</button>
-            </form> --}}
         </div>
+        <hr>
     </div>
-</footer>
+
+    <footer>
+        <div class="container py-5 d-flex justify-content-end align-items-center">
+            <div>
+                <a href="{{ route('admin.restaurant.edit', $restaurant) }}" class="btn btn-warning"><i
+                        class="fas fa-pencil me-2"></i>Modifica</a>
+            </div>
+        </div>
+    </footer>
 
 @endsection
-
-{{--      DA IMPLEMENTARE DELETE CONFIRMATION     --}}
-{{--Scripts
-@section('scripts')
-    @vite('resources/js/delete_confirmation.js')
-@endsection--}}
