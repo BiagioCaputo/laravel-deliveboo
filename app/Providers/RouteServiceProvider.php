547e41cd3410711a02b510d9c61dbb0bb46d951a
BiagioCaputo <scebiagio98@gmail.com>
2024-04-21 00:35:52 +0200

    Pagination added in dishes index

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Pagination\Paginator;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -36,5 +37,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        Paginator::useBootstrapFive();
     }
 }
