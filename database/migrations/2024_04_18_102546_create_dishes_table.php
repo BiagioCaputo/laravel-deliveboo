de260181b76a694a1368da8ab7bbe60ae20c9f8c
giamaf <gianluca.maffucci@hotmail.com>
2024-04-20 02:14:01 +0200

    added SoftDelete in Dish's Model and Table

--- database/migrations/2024_04_18_102546_create_dishes_table.php
+++ database/migrations/2024_04_18_102546_create_dishes_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->boolean('available')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
