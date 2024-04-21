8a80925013922c2da0e024214d82e983cf6c3336
LukePadawalker <padurariu.luca@gmail.com>
2024-04-21 16:59:39 +0200

    added section 2

--- resources/views/includes/layouts/content.blade.php
+++ resources/views/includes/layouts/content.blade.php
@@ -1,4 +1,21 @@
-<div class="container-fluid d-flex flex-column justify-content-evenly align-items-center flex-wrap">
-    <div class="h1">Il cibo che desideri</div>
+<div class="container-fluid section-1 d-flex flex-column justify-content-center align-items-center flex-wrap pb-5 mb-5">
+    <div class="h1 py-2">Il cibo che desideri</div>
     <div class="h1">A distanza di un click</div>
+</div>
+<div class="container section-2 d-flex justify-content-around pt-5 mt-5">
+    <div class="content-card d-flex flex-column align-items-center justify-content-start">
+        <img src="img/rider-image-opt.png">
+        <h3 class="pt-2">Diventa un rider</h3>
+        <p class="pt-2">Godi di flessibilità, libertà e guadagni competitivi con il servizio di consegna Glovo.</p>
+    </div>
+    <div class="content-card d-flex flex-column align-items-center justify-content-start">
+        <img src="img/partners-image-opt.png">
+        <h3 class="pt-2">Diventa un partner</h3>
+        <p class="pt-2">Cresci con Glovo! La nostra tecnologia e bacino di utenza può aiutarti ad acquisire nuovi clienti e sbloccare nuove opportunità!</p>
+    </div>
+    <div class="content-card d-flex flex-column align-items-center justify-content-start">
+        <img src="img/careers-image-opt.png">
+        <h3 class="pt-2">Avvia la tua carriera</h3>
+        <p class="pt-2">Pronto per una nuova sfida? Se sei ambizioso, umile e sei portato per il team working, vogliamo sentire la tua storia!</p>
+    </div>
 </div>
