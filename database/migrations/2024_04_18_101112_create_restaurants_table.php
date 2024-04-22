c97004b94bc6e6653158fb75b5b5d7e233e2ba88
BiagioCaputo <scebiagio98@gmail.com>
2024-04-22 15:29:10 +0200

    Now the user can add a logo to his restaurant

--- database/migrations/2024_04_18_101112_create_restaurants_table.php
+++ database/migrations/2024_04_18_101112_create_restaurants_table.php
@@ -20,7 +20,14 @@ return new class extends Migration
             $table->string('address', 100);
             $table->char('vat', 11)->unique();
             $table->string('email', 100)->unique();
+            $table->string('phone', 20)->nullable();
+
             $table->string('image')->nullable();
+            $table->string('logo')->nullable();
+
+            $table->time('opening_hour')->nullable();
+            $table->time('closing_hour')->nullable();
+            $table->string('opening_days')->nullable();
 
             $table->timestamps();
             $table->softDeletes();
