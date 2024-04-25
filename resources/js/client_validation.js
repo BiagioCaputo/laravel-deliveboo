77014472b63b771f11483acc099686425a33f676
giamaf <gianluca.maffucci@hotmail.com>
2024-04-25 12:39:30 +0200

    added custom email's input message

--- resources/js/client_validation.js
+++ resources/js/client_validation.js
@@ -15,8 +15,52 @@ const deleteButton = document.getElementById('modal-delete-confirmation');
 const backButton = document.getElementById('modal-back-button');
 const sendFormButton = document.getElementById('send-form-button');
 
+const emailSuggest = document.getElementById('email-suggest');
+
 // Preparo una variabile per conteggiare le tipologie scelte
 let counter = 0;
+let message;
+
+//# Funzione per validare il campo mail
+const validationEmail = () => {
+    emailField.addEventListener('blur', () => {
+
+        // Recupero il testo inserito dall'utente
+        const email = emailField.value.trim();
+        // console.log(email);
+
+        // Variabile per verificare se la mail include @, .it, .com
+        const isEmailOk = email.includes('@') && (email.includes('.it') || email.includes('.com'));
+
+        // Variabile per capire se c'è matching tra il testo e le mail a sistema
+        let isEmailAvailable = true;
+
+        // Se c'è del testo ed è ok
+        if (email && isEmailOk) {
+            for (const user of users) {
+                // Se c'è matching allora
+                if (user.email === email) {
+                    isEmailAvailable = false;
+                    break;
+                }
+            }
+
+            // Se l'indirizzo mail non è stato ancora registrato a sistema
+            if (isEmailAvailable) {
+                message = `<span class="text-success">Indirizzo mail valido</span>`;
+            } else {
+                message = `<span class="text-danger">Questo indirizzo email è già in uso. Prova il reset password.</span>`;
+            }
+        }
+        // Se non c'è testo e quest'ultimo non include @, .it, .com
+        else {
+            message = `<span class="text-danger">Inserire un indirizzo mail valido.</span>`;
+        }
+
+        // Stampo il messaggio
+        emailSuggest.innerHTML = message;
+    });
+};
 
 //# Funzione per verificare se è stata selezionata almeno una tipologia di ristorante
 const hasTypes = () => {
@@ -36,8 +80,8 @@ const hasTypes = () => {
     });
 }
 
-const showModal = () => {
-
+//# Funzione per preparare la modale
+const makeModal = () => {
     // Preparo la modale
     deleteButton.classList.add('d-none');
     backButton.innerText = 'Ho capito!';
@@ -45,6 +89,11 @@ const showModal = () => {
     modalBody.classList.add('my-2');
     modalBody.innerText = 'Per poter procedere con la registrazione è necessario inserire tutti i dati richiesti!';
 }
+
+//! SVOLGIMENTO -------------------------------------------------------------------------
+
+// Chiamo la funzione per validare il campo mail
+validationEmail();
 
 // Chiamo la funzione che verifica se c'è almeno una tipologia
 hasTypes();
@@ -61,4 +110,3 @@ form.addEventListener('submit', e => {
         form.submit();
     }
 })
-
