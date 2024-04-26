09cf2a6c28870754c1b9c63010f400a6998c8e74
Axhu99 <xhuliodreni@icloud.com>
2024-04-26 11:09:23 +0200

    Api-restaurant

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\RestaurantController;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('restaurants', RestaurantController::class); //RISORSE API per i ristoranti
