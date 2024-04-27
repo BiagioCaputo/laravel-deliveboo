634b9078260eaf083b4198c9f601f07cd95bb14d
Axhu99 <xhuliodreni@icloud.com>
2024-04-27 15:47:31 +0200

    image type and fix api

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,5 +16,10 @@ class Type extends Model
     public function restaurants()
     {
         return $this->belongsToMany(Restaurant::class);
+    }
+    //funzione per montare la stringa per le immagini cosi da rendere la vita facile al front
+    public function image(): Attribute
+    {
+        return Attribute::make(fn ($value) => $value && app('request')->is('api/*') ? url('storage/' . $value) : $value);
     }
 }
