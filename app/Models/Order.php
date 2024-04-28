bba25373d17ae37cbd00ebea8276b21eb73c108b
BiagioCaputo <scebiagio98@gmail.com>
2024-04-28 22:14:23 +0200

    order show layout created

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,5 +18,11 @@ class Order extends Model
     public function dishes()
     {
         return $this->belongsToMany(Dish::class);
+    }
+
+    //funzione per cambiare il format delle date
+    public function getFormattedDate($column, $format = 'd-m-Y')
+    {
+        return Carbon::create($this->$column)->format($format);
     }
 }
