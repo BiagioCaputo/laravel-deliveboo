e4e35f563652abfd9272b3b3f9df6ed8bd4dae4b
BiagioCaputo <scebiagio98@gmail.com>
2024-04-28 21:53:51 +0200

    order show created

---
+++ resources/views/admin/orders/show.blade.php
@@ -0,0 +1,25 @@
+{{-- Layout --}}
+@extends('layouts.app')
+
+{{-- Titolo --}}
+@section('title', 'Ordini')
+
+{{-- Contenuto principale pagina --}}
+@section('content')
+
+    <header>
+        <h1 class="text-center my-3">Dettagli Ordine</h1>
+    </header>
+
+    <div class="container py-2">
+
+       
+    </div>
+
+@endsection
+
+
+{{-- Scripts --}}
+@section('scripts')
+
+@endsection
