63b32628108cbded03594a32b9d267c01e7061c8
giamaf <gianluca.maffucci@hotmail.com>
2024-05-03 19:21:36 +0200

    make gateway singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Il singleton permette di creare una sola istanza di Gateway che può accedere a tutta l'app
+        $this->app->singleton(Gateway::class, function ($app) {
+
+            return  new Gateway(
+                [
+                    'environment' => env('BRAINTREE_ENV'),
+                    'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+                    'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+                    'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+                ]
+            );
+        });
     }
 }
