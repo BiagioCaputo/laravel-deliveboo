50a2985cd32596b296c37cf0ef9eb826649d6fd9
BiagioCaputo <scebiagio98@gmail.com>
2024-05-04 15:36:36 +0200

    Added courses available in a specific restaurant

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -19,13 +19,6 @@ class RestaurantController extends Controller
         return response()->json($restaurants);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -35,26 +28,15 @@ class RestaurantController extends Controller
 
         if (Dish::whereRestaurant_id($id)->get() == '[]') return response(null, 404);
 
+        // Cerco le tipologie del ristorante
         $restaurant = Restaurant::with('types')->find($id);
 
-        $dishes = Dish::whereRestaurant_id($id)->whereAvailable(true)->with('course')->orderBy('name')->get(); //cerco i piatti con l'id del ristorante
+        // Cerco i piatti con l'id del ristorante
+        $dishes = Dish::whereRestaurant_id($id)->whereAvailable(true)->with('course')->orderBy('name')->get();
 
-        return response()->json(['dishes' => $dishes, 'restaurant' => $restaurant]);
-    }
+        // Recupera le portate solo dei piatti presenti nella show del ristorante
+        $courses = $dishes->pluck('course')->unique()->values()->all();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Restaurant $restaurant)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Restaurant $restaurant)
-    {
-        //
+        return response()->json(['dishes' => $dishes, 'restaurant' => $restaurant, 'courses' => $courses]);
     }
 }
