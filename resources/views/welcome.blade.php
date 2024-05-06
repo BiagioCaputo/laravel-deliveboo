17247fdf43d41acb5d0252a5b1ffd79363cb0c43
Axhu99 <xhuliodreni@icloud.com>
2024-05-06 18:49:00 +0200

    responsive home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,19 +8,21 @@
 
 @section('content')
 
-{{-- Sezione introduttiva --}}
-<div class="jumbotron d-flex justify-content-center align-items-center">
-    <div class="container d-flex justify-content-center align-items-center">
-        <img src="img/glovo_logo.png" alt="logo glovo">
-        <div>
-            <h1>Le tue funzionalità <br> in un'unica App </br> </h1>
-            <p>Scopri tutte le funzionalità che DeliveBoo offre ai propri partner!</p>
-            <a class="btn btn-primary me-1" href="{{ url('/register') }}">Registrati</a>
-            <a class="btn btn-primary" href="{{ url('/login') }}">Accedi</a>
+    {{-- Sezione introduttiva --}}
+    <div class="jumbotron d-flex justify-content-center align-items-center">
+        <div class="container d-flex justify-content-center align-items-center">
+            <div class="row">
+                <img src="img/glovo_logo.png" alt="logo glovo" class="d-md-inline col-md-6 col-lg-4">
+                <div class="col-md-6 col-lg-8">
+                    <h1>Le tue funzionalità <br> in un'unica App </br> </h1>
+                    <p>Scopri tutte le funzionalità che DeliveBoo offre ai propri partner!</p>
+                    <a class="btn btn-primary me-1" href="{{ url('/register') }}">Registrati</a>
+                    <a class="btn btn-primary" href="{{ url('/login') }}">Accedi</a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-<img src="img/jumbotron-wave-desktop.svg" alt="Wave" class="wave">
+    <img src="img/jumbotron-wave-desktop.svg" alt="Wave" class="wave">
 
 
 @endsection
