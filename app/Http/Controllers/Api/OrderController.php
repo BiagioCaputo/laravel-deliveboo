4bc70e7e3e31004d6a2b1aaf86305f7edd61838b
giamaf <gianluca.maffucci@hotmail.com>
2024-05-06 10:31:20 +0200

    updated OrderController for testing

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Braintree\Gateway;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Orders\OrderRequest;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -14,26 +15,49 @@ class OrderController extends Controller
         // Recupero il token generato
         $token = $gateway->clientToken()->generate();
 
-        // Mando al client il token
+        // $nonce = $gateway->paymentMethodNonce()->create($request->input('paymentMethodToken'));
+        // 'paymentMethodNonce' => $nonce->nonce
+        // Mando al client il token e il
         return response()->json($token, 200);
     }
 
-    public function makePayment(OrderRequest $request, Gateway $gateway)
+    public function makePayment(Request $request, Gateway $gateway)
     {
+        // var_dump($request->input());
+        // dd($request->input('amount'));
+        // dd($request->getContent());
         $nonceFromTheClient = $request->input('payment_method_nonce');
+
         // Creo la transazione
         // TODO - Integrare con il prezzo totale
-        $result = $gateway->transaction()->sale([
+        $transaction = $gateway->transaction()->sale([
             'amount' => '10.00',
             'paymentMethodNonce' => $nonceFromTheClient,
-            'deviceData' => $request,
+            'customer' => [
+                'firstName' => $request->input('customer_name'),
+                'email' => $request->input('customer_email'),
+                'phone' => $request->input('customer_phone_number'),
+            ],
+            'shipping' => [
+                'firstName' => $request->input('customer_name'),
+                'streetAddress' => $request->input('customer_address')
+            ],
+            // 'paymentDetails' => [
+            //     'customer_name' => $request->input('customer_name', 'Gianluca'),
+            //     'customer_address' => $request->input('customer_address'),
+            //     'customer_email' => $request->input('customer_email'),
+            //     'customer_phone_number' => $request->input('customer_phone_number'),
+            //     'dishes' => $request->input('dishes'),
+            //     'restaurant_id' => $request->input('restaurant_id')
+            // ],
             'options' => [
-                'submitForSettlement' => True
+                // 'submitForSettlement' => True
             ]
         ]);
+        dd($transaction);
 
-        // Restituzione della risposta con i dati della transazione
-        if ($result->success) {
+        // Restituzione della risposta
+        if ($transaction->success) {
             $data = [
                 'success' => true,
                 'message' => 'Transazione eseguita con successo'
