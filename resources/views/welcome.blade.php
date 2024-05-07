3d120487c677cd8e2c9b2a71e64f657d50d78de9
EricaMarzi <erica.marzi.1993@gmail.com>
2024-05-07 17:45:49 +0200

    wave responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,21 +8,22 @@
 
 @section('content')
 
-    {{-- Sezione introduttiva --}}
-    <div class="jumbotron d-flex justify-content-center align-items-center">
-        <div class="container d-flex justify-content-center align-items-center">
-            <div class="row">
-                <img src="img/glovo_logo.png" alt="logo glovo" class="d-md-inline col-md-6 col-lg-4">
-                <div class="col-md-6 col-lg-8">
-                    <h1>Le tue funzionalità <br> in un'unica App </br> </h1>
-                    <p>Scopri tutte le funzionalità che DeliveBoo offre ai propri partner!</p>
-                    <a class="btn btn-primary me-1" href="{{ url('/register') }}">Registrati</a>
-                    <a class="btn btn-primary" href="{{ url('/login') }}">Accedi</a>
-                </div>
+{{-- Sezione introduttiva --}}
+<div class="jumbotron d-flex justify-content-center align-items-center">
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="row">
+            <img src="img/glovo_logo.png" alt="logo glovo" class="d-md-inline col-md-6 col-lg-4">
+            <div class="col-md-6 col-lg-8">
+                <h1>Le tue funzionalità <br> in un'unica App </br> </h1>
+                <p>Scopri tutte le funzionalità che DeliveBoo offre ai propri partner!</p>
+                <a class="btn btn-primary me-1" href="{{ url('/register') }}">Registrati</a>
+                <a class="btn btn-primary" href="{{ url('/login') }}">Accedi</a>
             </div>
         </div>
     </div>
-    <img src="img/jumbotron-wave-desktop.svg" alt="Wave" class="wave">
+</div>
+<img src="img/jumbotron-wave-desktop.svg" alt="Wave" class="wave" id="desktop-wave">
+<img src="img/down-wave-mobile.svg" alt="Wave" class="wave" id="mobile-wave">
 
 
 @endsection
