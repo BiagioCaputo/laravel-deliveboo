b377f25354da16bb78a50d3f55d13544fcc5a169
EricaMarzi <erica.marzi.1993@gmail.com>
2024-05-07 17:22:32 +0200

    responsive order page

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -23,10 +23,10 @@
             <thead>
                 <tr>
                     <th scope="col">Id ordine</th>
-                    <th scope="col">Nome cliente</th>
-                    <th scope="col" class="order-992">Indirizzo cliente</th>
-                    <th scope="col">Email cliente</th>
-                    <th scope="col">Telefono cliente</th>
+                    <th scope="col" class="order-768">Nome cliente</th>
+                    <th scope="col" class="order-992">>Indirizzo cliente</th>
+                    <th scope="col" class="order-992">Email cliente</th>
+                    <th scope="col" class="order-768">Telefono cliente</th>
                     <th scope="col">Prezzo</th>
                     <th scope="col">Pagato</th>
                     <th scope="col">Data ordine</th>
@@ -37,11 +37,11 @@
                 @forelse ($orders as $order)
                 <tr>
                     <th scope="row">{{ $order->id }}</th>
-                    <td>{{ $order->customer_name }}>
+                    <td class="order-768">{{ $order->customer_name }}
                     </td>
-                    <td class="order-992">{{ $order->customer_address }}</td>
-                    <td>{{ $order->customer_email }}</td>
-                    <td>{{ $order->customer_phone }}</td>
+                    <td class="order-992">>{{ $order->customer_address }}</td>
+                    <td class="order-992">{{ $order->customer_email }}</td>
+                    <td class="order-768">{{ $order->customer_phone }}</td>
                     <td>{{ $order->total_price }}</td>
                     <td class="text-center">{{ $order->status ? 'Sì' : 'No' }}</td>
                     <td>{{ $order->created_at }}</td>
